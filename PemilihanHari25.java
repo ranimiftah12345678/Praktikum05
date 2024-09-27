92ab24136d0a4e413f1401ebb75d6391b116733e
ranimiftah12345678 <ranimiptahhh@gmail.com>
2024-09-27 10:17:55 +0700

    commit kedua

--- PemilihanHari25.java
+++ PemilihanHari25.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class PemilihanHari25 {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String dayName, dayType;
         System.out.print("Input day name: ");
